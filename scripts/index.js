ce8b19a04ed30e7698bff45432d0630a9ca2e70e
Artemilyen <Artemilyen@gmail.com>
2022-07-29 18:56:24 +0300

    fixed bugs after first review

--- scripts/index.js
+++ scripts/index.js
@@ -2,34 +2,41 @@ let editButton = document.querySelector(".profile__edit-button");
 // let addButton = document.querySelector(".profile__add-button");
 let closeButton = document.querySelector(".popup__close-icon");
 let saveForm = document.querySelector(".popup__form");
-let inputName = document.querySelector(".popup__name");
-let inputProfession = document.querySelector(".popup__profession");
+let inputName = document.querySelector(".popup__input_info_name");
+let inputProfession = document.querySelector(".popup__input_info_about");
 let profileName = document.querySelector(".profile__name");
 let profileProfession = document.querySelector(".profile__profession");
 let popup = document.querySelector(".popup");
 
 function openPopup() {
-  popup.classList.remove("popup_hidden");
+  popup.classList.add("popup_opened");
+  saveProfileInfo();
 }
-
-editButton.addEventListener("click", openPopup);
 
 function closePopup() {
-  popup.classList.add("popup_hidden");
+  popup.classList.remove("popup_opened");
 }
-
-closeButton.addEventListener("click", closePopup);
 
 function saveProfileInfo() {
   inputName.value = profileName.textContent;
   inputProfession.value = profileProfession.textContent;
 }
 
-saveProfileInfo();
-
-saveForm.addEventListener("submit", function (evt){
-    evt.preventDefault()
+function submitForm(evt){
+  evt.preventDefault();
   profileName.textContent = inputName.value;
   profileProfession.textContent = inputProfession.value;
   closePopup();
-});
+}
+
+editButton.addEventListener("click", openPopup);
+
+closeButton.addEventListener("click", closePopup);
+
+saveForm.addEventListener("submit", submitForm);
+
+// saveForm.addEventListener("submit", function(evt){
+//     evt.preventDefault();
+//     submitForm();
+//     console.log('попозже отправим');
+// });
